0a141e739a8e5e24629c0a416ba19e07d7874e25
Koopa <codingkoopa@gmail.com>
2020-02-28 22:55:05 -0500

    Finalize button and axis mapping.

--- src/main/java/frc/robot/DriveStation.java
+++ src/main/java/frc/robot/DriveStation.java
@@ -21,19 +21,31 @@ public final class DriveStation {
   public static final int kIDButtonX = 3;
   /** The button ID of the Y button. */
   public static final int kIDButtonY = 4;
-  /** The button ID of the RB button, the right bumper. */
+  /** The button ID of LB, the right bumper. */
+  public static final int kIDButtonLB = 5;
+  /** The button ID of RB, the right bumper. */
   public static final int kIDButtonRB = 6;
+  /** The button ID of the back button, also select on some controllers. */
+  public static final int kIDButtonBack = 7;
   /** The button ID of the start button. */
   public static final int kIDButtonStart = 8;
+  /** The button ID of LS, activated by clicking the left stick. */
+  public static final int kIDButtonLS = 9;
+  /** The button ID of RS, activated by clicking the right stick. */
+  public static final int kIDButtonRS = 10;
 
   // Axis IDs
 
+  /** The axis ID of the X dimension of the left analog stick. */
+  public static final int kIDAxisLeftX = 0;
   /** The axis ID of the Y dimension of the left analog stick. */
   public static final int kIDAxisLeftY = 1;
-  /** The axis ID of the X dimension of the right analog stick. */
-  public static final int kIDAxisRightX = 2;
   /** The axis ID of LT, the left trigger. */
-  public static final int kIDAxisLT = 4;
+  public static final int kIDAxisLT = 2;
   /** The axis ID of RT, the right trigger. */
-  public static final int kIDAxisRT = 5;
+  public static final int kIDAxisRT = 3;
+  /** The axis ID of the X dimension of the right analog stick. */
+  public static final int kIDAxisRightX = 4;
+  /** The axis ID of the Y dimension of the right analog stick. */
+  public static final int kIDAxisRightY = 5;
 }
