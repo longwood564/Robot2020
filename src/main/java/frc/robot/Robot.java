3a001137c0f05ef79b034d458f38543c5cfc7bd1
Koopa <codingkoopa@gmail.com>
2020-02-28 22:55:42 -0500

    Move compressor code to more appropriate places.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -194,7 +194,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    setCompressor();
+    // Set the compressor in closed loop control to enable it.
+    compressor.setClosedLoopControl(true);
 
     // Slave follows master
     m_motorDriveBackRight.follow(m_motorDriveFrontRight);
@@ -318,20 +319,13 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * Sets the compressor in closed loop control to toggle when it is enabled.
-   */
-  private void setCompressor() {
-    // Set the compressor in closed loop control to enable it.
-    compressor.setClosedLoopControl(true);
-    compressor.start();
-  }
-
-  /**
    * Initializes autonomous mode.
    */
   @Override
   public void autonomousInit() {
     m_selectedAuto = m_autoChooser.getSelected();
+
+    compressor.start();
   }
 
   /**
@@ -358,6 +352,8 @@ public class Robot extends TimedRobot {
     m_lastDetectedColorString = "N/A";
     m_targetControlPanelColor = "N/A";
     m_controlPanelSpinAmount = 0;
+
+    compressor.start();
   }
 
   /**
