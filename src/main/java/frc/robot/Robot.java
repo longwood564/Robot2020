8c62f98db7667026b76c5c05a16dd4e2f6ea0975
RySty <ryanstyron1220@gmail.com>
2020-03-07 08:50:56 -0500

    Add limit sensor code for the winch.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -87,6 +87,7 @@ public class Robot extends TimedRobot {
   private final DoubleSolenoid m_doubleSolenoidWinch =
       new DoubleSolenoid(RoboRIO.kPortDoubleSolenoidForwardWinch,
           RoboRIO.kPortDoubleSolenoidBackwardWinch);
+  private final DigitalInput m_limitSwitchSensorWinch = new DigitalInput(2);
   private boolean m_raiseWinch = false;
 
   // Launching
@@ -501,21 +502,20 @@ public class Robot extends TimedRobot {
 
   /**
    * Toggles whether or not the winch is raised or declined.
-   * 
-   * TODO: Verify this is what needs to be done with the winch.
-   * 
    */
   private void toggleWinch() {
     if (m_buttonDrivePressA) {
       m_raiseWinch = !m_raiseWinch;
     }
 
-    if (m_raiseWinch) {
-      m_doubleSolenoidWinch.set(DoubleSolenoid.Value.kForward);
-      m_motorWinch.set(1);
-    } else {
-      m_doubleSolenoidWinch.set(DoubleSolenoid.Value.kReverse);
-      m_motorWinch.set(0);
+    if (!m_limitSwitchSensorWinch.get()) {
+      if (m_raiseWinch) {
+        m_doubleSolenoidWinch.set(DoubleSolenoid.Value.kForward);
+        m_motorWinch.set(1);
+      } else {
+        m_doubleSolenoidWinch.set(DoubleSolenoid.Value.kReverse);
+        m_motorWinch.set(0);
+      }
     }
   }
 
