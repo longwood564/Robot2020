2e2bbe072f5802daee2d468c9019b969d5807160
RySty <ryanstyron1220@gmail.com>
2021-02-04 15:09:30 -0500

    Tune the intake and belt motor speeds.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -78,8 +78,7 @@ public final class Constants {
   // TODO: Tune this value.
   public static final double kSpeedControlPanel = 0.25;
   /** The speed to set the intake motor to. */
-  // TODO: Tune this value.
-  public static final double kSpeedIntake = 0.75;
+  public static final double kSpeedIntake = 0.9;
   /** The speed to set the intake motor to. */
   public static final double kSpeedLauncher = 1.0;
   /** The speed to set the belt motor to. */
